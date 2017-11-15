a4bef703537a621b20dfae632b59d854afcc590c
Jason Vance <jbvance@gmail.com>
2017-11-15 15:17:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,11 +14,85 @@ class TicTacToe
   end
 
   def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def move(index, current_player="X")
+    @board[index] = current_player
+  end
+
+  def position_taken?(index)
+  !(@board[index].nil? || @board[index] == " ")
+  end
+
+  def valid_move?(index)
+    if index.between?(0,8)
+      !position_taken?(@board, index) ? true : false
+    end
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player
+      display_board
+    else
+      turn
+    end
+  end
+
+  def turn_count
+    turns = 0
+    @board.each do |token|
+      if token == "X" || token == "O"
+        turns += 1
+      end
+    end
+    turns
+  end
+
+  def current_player
+    turn_count(@board) % 2 == 0 ? "X" : "O"
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |arr|
+      if @board[arr[0]] != " " && @board[arr[0]] == @board[arr[1]] && @board[arr[1]] == @board[arr[2]]
+        return arr
+      end
+    end
+    return false
+  end
+
+  def full?
+    !@board.any? { |a| (a == " ") }
+  end
+
+  def draw?
+    full? && !won?
+  end
+
+  def over?
+    won? || draw? || full?
+  end
+
+  def winner
+    winning_arr = won?
+    if winning_arr
+      @board[winning_arr[0]]
+    else
+      nil
+    end
+  end
+  
 end
